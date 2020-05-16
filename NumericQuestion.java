277dc4c280c78153f20f789cccdc497cfa0b2fb6
cisnerosya <61475778+cisnerosya@users.noreply.github.com>
2020-05-15 18:26:13 -0700

    Update NumericQuestion.java

--- NumericQuestion.java
+++ NumericQuestion.java
@@ -1,3 +1,11 @@
+/*
+File: NumericQuestion.java
+Description: This adds source code to a previous answer.
+Created: May 14th, 2020
+Author: Yao Cisneros
+email: yaoitzc7054@student.vvc.edu
+*/
+
 public class NumericQuestion extends Question
 {
 	private double answer;
